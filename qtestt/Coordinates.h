fe4e837de1258f2fcb2aec0d611f5e4cd553cb5a
A-lx-is <alexis.beriault.1@gmail.com>
2025-03-18 13:20:52 -0400

    Update Coordinates.h

--- qtestt/Coordinates.h
+++ qtestt/Coordinates.h
@@ -1,16 +1,14 @@
 #ifndef COORDINATES_H
 #define COORDINATES_H
 
-struct Coordinates{
-    int x1;
-    int y1;
-    int x2;
-    int y2;
-};
-
-struct Size2{
+typedef struct {
     int x;
     int y;
-};
+} Coordinates;
+
+typedef struct {
+    int x;
+    int y;
+} Size2;
 
 #endif // COORDINATES_H
