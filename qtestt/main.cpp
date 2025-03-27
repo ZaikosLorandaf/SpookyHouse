0255cc21415c311c1cb7ecdb84c33c86fd71f3a0
A-lx-is <alexis.beriault.1@gmail.com>
2025-03-27 14:04:15 -0400

    actual movement

--- qtestt/main.cpp
+++ qtestt/main.cpp
@@ -16,22 +16,23 @@ const char* temp[] = {
     "####                                                                 #                                               #",
     "####                                                                 #                                               #",
     "####                                                                 #                                               #",
-    "####                                                                 #                    #!###                      #",
+    "####                 #                                               #                    #!###                      #",
     "####                                                                 #                    #####                      #",
     "####                                                                 #                                               #",
     "####                                                                 #################################################",
     "####                                                                 #                                                ",
     "####                                                                 #                                                ",
     "####                                                                 #                                                ",
-    "####                                                                 X1                                               ",
+    "####     #                                                           X1                                               ",
     "####                                                                 #                                                ",
     "######################################################################                                                "};
 
 void updateGUI(MainWindow* w, Coordinates pc){
+    //TODO: cette fonction a VRAIMENT besoin d'optimisation.
     //range to start loading images
     int range = 10;
-    //range to start unloading images
-    int unload = 12;
+
+
     //getting a list of all labels
     std::vector<QLabel*> labelList;
     QObjectList l = w->children();
@@ -40,41 +41,62 @@ void updateGUI(MainWindow* w, Coordinates pc){
             labelList.push_back(qobject_cast<QLabel*>(c));
         }
     }
+
+
     //unloading images further than %unload tiles away
+    //TODO: i made this delete every image because i messed
+    //up and the loading images part would duplicate images
+    //which is bad.
+
+    //range to start unloading images
+//    int unload = 12;
     if (!labelList.empty()){
         for (int k=0; k<labelList.size(); k++){
-            if (labelList[k]->x() - pc.x > unload || labelList[k]->y() - pc.y > unload){
+//            if (abs(labelList[k]->x() - pc.x) > unload || abs(labelList[k]->y() - pc.y) > unload){
                 delete(labelList[k]);
-            }
+//            }
         }
     }
+    labelList.erase(std::remove(labelList.begin(), labelList.end(), nullptr), labelList.end());
+
     //defines what a wall '#' looks like
     const char* path = ":/assets/images/cat.png";
-    Size2 tailleMur;
-    tailleMur.x = 25;
-    tailleMur.y = 25;
 
-    //loading images less than %range tiles away
+    Size2 tailleMur;
+    tailleMur.x = 33;
+    tailleMur.y = 33;
+
+    //loading images less than %range tiles away, centered on the player
     for (int i= -range; i<range; i++){
         for (int j=-range; j<range; j++){
-            if (i+pc.y>0 && j+pc.x>0 && j+pc.x<118 && i+pc.y<21){
-                if (temp[i+pc.y][j+pc.x] == '#'){
-                    Coordinates c;
-                    c.y = (i+pc.y)*tailleMur.x;
-                    c.x = (j+pc.x)*tailleMur.y;
-                    QLabel* image = w->createImageLabelFromPath(path, tailleMur, c);
-                    image->show();
-                }
+            if (i+pc.y>0 && j+pc.x>0 && j+pc.x<118 && i+pc.y<21 && temp[i+pc.y][j+pc.x] == '#'){
+
+                Coordinates c;
+                c.y = (i+9)*tailleMur.y;
+                c.x = (j+12)*tailleMur.x;
+
+                QLabel* image = w->createImageLabelFromPath(path, tailleMur, c);
+                image->show();
+
             }
         }
     }
+
+    //display character
+    Coordinates c2;
+    c2.x = 12*tailleMur.x;
+    c2.y = 8*tailleMur.y;
+    QLabel* image = w->createImageLabelFromPath(":/assets/images/lowtiergod.jpg", tailleMur, c2);
+    image->show();
 }
 
 void move(MainWindow* w, int key) {
+//Lorsqu'on recoit un user input. Peut-etre meme pas un mouvement mais une interaction.
 
-
-    qDebug() << "Key pressed:" << w->pc.x;
-    qDebug() << "Key pressed:" << w->pc.y;
+//Tests.
+//    qDebug() << "posx" << w->pc.x;
+//    qDebug() << "posy" << w->pc.y;
+//    qDebug() << key;
     /*
      * W: 87
      * A: 65
@@ -86,27 +108,25 @@ void move(MainWindow* w, int key) {
 
     switch (key){
     case 87:
-        if (w->pc.y -= 1 >=0 && temp[w->pc.x][w->pc.y -=1] != '#'){
+        if (w->pc.y - 1 >=0 && temp[w->pc.x][w->pc.y -1] != '#'){
             w->pc.y -= 1;
-
         }
+//        w->q->move(w->q->x(),w->q->y()+10);
         break;
     case 83:
-        if (w->pc.y += 1 >=0 && temp[w->pc.x][w->pc.y +=1] != '#'){
+        if (w->pc.y + 1 >=0 && w->pc.y + 1 < 21 && temp[w->pc.x][w->pc.y +1] != '#'){
             w->pc.y += 1;
-
         }
+//        w->q->move(w->q->x(),w->q->y()-10);
         break;
     case 65:
-        if (w->pc.x -= 1 >=0 && temp[w->pc.x-=1][w->pc.y] != '#'){
+        if (w->pc.x - 1 >=0 && temp[w->pc.x-1][w->pc.y] != '#'){
             w->pc.x -= 1;
-
         }
         break;
     case 68:
-        if (w->pc.x += 1 >=0 && temp[w->pc.x+=1][w->pc.y] != '#'){
+        if (w->pc.x + 1 >=0 && w->pc.x + 1 < 121 && temp[w->pc.x+1][w->pc.y] != '#'){
             w->pc.x += 1;
-
         }
         break;
     case 81:
@@ -119,63 +139,16 @@ void move(MainWindow* w, int key) {
 }
 
 void createMap(MainWindow *w){
-
-
-//    Size2 tempSize;
-//    tempSize.x = 118;
-//    tempSize.y = 21;
-//
-//    const char* path = ":/assets/images/cat.png";
-//    Size2 tailleMur;
-//    tailleMur.x = 25;
-//    tailleMur.y = 25;
-//
-//    QLabel* map[118*21];
-//    for (int i=0; i<tempSize.y; i++)
-//        for (int j=0; j<tempSize.x; j++){
-//            if (temp[i][j] == '#'){
-//                Coordinates c;
-//                c.y = i*tailleMur.x;
-//                c.x = j*tailleMur.y;
-//                QLabel* image = w->createImageLabelFromPath(path, tailleMur, c);
-//                map[i*tempSize.x+j] = image;
-//                image->show();
-//            }
-//
-//        }
-//
     move(w, -1);
     return;
 }
 
-void buttonPushedImageCat(QPushButton *p, MainWindow *w){
+void buttonPushed(QPushButton *p, MainWindow *w){
+    //Lorsque le bouton Start Game est actionne
     delete p;
     w->clearTitleScreen();
     createMap(w);
-    //test pour vérifier le fonctionnement du buton. spoiler: il marche.
-//    const char* path2 = ":/assets/images/rat.png";
-//    QDialog *dialog = new QDialog(w);
-//    dialog->setWindowTitle(" ");
-//
-//   QLabel *label = new QLabel(dialog);
-//    QPixmap pixmap(path2);
-//    label->setPixmap(pixmap);
-//    label->setScaledContents(true);
-//
-//    QVBoxLayout *layout = new QVBoxLayout(dialog);
-//    layout->addWidget(label);
-//    dialog->setLayout(layout);
-//
-//    dialog->setFixedSize(pixmap.size());
-//    dialog->exec();
-
 }
-
-
-
-
-
-
 
 int main(int argc, char *argv[])
 {
@@ -191,13 +164,9 @@ int main(int argc, char *argv[])
     size.x = 256;
     size.y = 75;
 
-    Coordinates pc;
-    pc.x = 5;
-    pc.y = 0;
-
     w.createPushButton(":/assets/images/StartGame001.png", size, coord);
 
-    QObject::connect(&w, &MainWindow::buttonClicked, buttonPushedImageCat);
+    QObject::connect(&w, &MainWindow::buttonClicked, buttonPushed);
     QObject::connect(&w, &MainWindow::keyPressed, move);
 
     return a.exec();
