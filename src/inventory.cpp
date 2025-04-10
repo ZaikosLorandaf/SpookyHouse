a511e3f8619843a7b90508bcb2a19b8e86eb09cd
Deer Canidae <deer.canidae@gmail.com>
2025-04-09 22:24:54 -0400

    added missing implementation in inventory.cpp

--- src/inventory.cpp
+++ src/inventory.cpp
@@ -41,3 +41,40 @@ void Inventory::store_key_vector(std::vector<std::string> &&key_vec) {
   sr.init_key_value(std::string("KEY_LIST"), std::move(buff));
   SaveManager::set_reccord(Inventory::type, Inventory::key_id, std::move(sr));
 }
+
+void Inventory::pickup_key(const char *key_name) {
+  auto kv = retrieve_key_vector();
+  for (const auto &key : kv) {
+    if (key == key_name)
+      return;
+  }
+  kv.emplace_back(key_name);
+  store_key_vector(std::move(kv));
+}
+
+bool Inventory::has_key(const char *key_name) {
+  auto kv = retrieve_key_vector();
+  for (const auto &key : kv) {
+    if (key == key_name)
+      return true;
+  }
+  return false;
+}
+
+void Inventory::drop_key(const char *key_name) {
+  auto kv = retrieve_key_vector();
+  std::size_t index = 0;
+  bool found;
+  for (auto it = kv.cbegin(); it != kv.cend(); it++) {
+    if (*it == key_name) {
+      kv.erase(it);
+      store_key_vector(std::move(kv));
+      return;
+    }
+  }
+}
+
+void Inventory::drop_all_key() {
+  std::vector<std::string> empty_vec;
+  store_key_vector(std::move(empty_vec));
+}
